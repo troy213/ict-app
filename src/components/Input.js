c5b5fa4bd909eb3bcabd7d4c55de31c0dde3ccfe
Tritera_Erlangga-Niomic <tritera.erlangga@gmail.com>
2021-02-28 00:39:30 +0700

    Eliminate unneccessary component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import Output from './Output';
 import { connect } from 'react-redux';
 
 class Input extends Component {
   constructor(props){
     super(props);
     this.handler = this.handler.bind(this);
+    this.proses = this.proses.bind(this);
+    this.prosesQry = this.prosesQry.bind(this);
     this.child = React.createRef();
   }
 
@@ -16,12 +17,81 @@ class Input extends Component {
     console.log(value+": "+e);
   }
 
-  prosesQry = () => {
-    this.child.current.prosesQry();
-  };
+  proses(){
+    var a = this.props.area;
+    var b = this.props.kode;
+    var c = Number(this.props.noAwal);
+    var d = Number(this.props.noAkhir);
 
-  proses = () => {
-    this.child.current.proses();
+    if (c>d) {
+      this.props.handleError("Nomor awal lebih besar dari nomor akhir!");
+    } else if (b==="" || c==="" || d==="") {
+      this.props.handleError("kolom masih kosong!");
+    } else {
+      for (var i = c; i <= d; i++) {
+        document.getElementById("out").innerHTML += b + i + "\t" + a +"\n";
+      }
+      document.getElementById("logger").innerHTML = "";
+    }
+  }
+
+  proses2(b,c,d){
+    var a = this.props.area;
+
+    if (c>d) {
+      this.props.handleError("Nomor awal lebih besar dari nomor akhir!");
+    } else if (b==="" || c===0 || d===0) {
+      this.props.handleError("kolom masih kosong!");
+    } else {
+      for (var i = c; i <= d; i++) {
+        document.getElementById("out").innerHTML += b + i + "\t" + a +"\n";
+      }
+      document.getElementById("logger").innerHTML = "";
+    }
+  }
+
+  clr(){
+    var clr = "";
+    document.getElementById("out").innerHTML = clr;
+    document.getElementById("in").innerHTML = clr;
+    document.getElementById("logger").innerHTML = "";
+    console.log("Clear");
+  }
+
+  prosesQry() {
+    var qry = this.props.in;
+    var etr = String.fromCharCode(10);
+    var res = qry.split(etr);
+    var x;
+    var y;
+
+    if (qry==="") {
+      this.props.handleError("query masih kosong");
+    } else {
+      for (var i = 0; i < res.length; i++) {
+        var j = 0;
+        x = res[i].indexOf("-");
+        y = res[i].length;
+        while (isNaN(res[i].substr(j,1))) {
+          j++;
+        }
+        var kode = res[i].substr(0,j);
+        var noAwal = Number(res[i].substr(j,x-j));
+        var noAkhir = Number(res[i].substr(x+1,y-x-1));
+
+        this.handler("kode",kode);
+        this.handler("noAwal",noAwal)
+        this.handler("noAkhir",noAkhir);
+        this.proses2(kode,noAwal,noAkhir);
+      }
+    }
+  }
+
+  copyContent(){
+    document.getElementById("out").removeAttribute("disabled");
+    document.getElementById("out").select();
+    document.execCommand('copy');
+    document.getElementById("out").setAttribute("disabled","");
   }
 
   render(){
@@ -142,16 +212,14 @@ class Input extends Component {
           </>
         )}
 
-        <Output
-          area={this.props.area}
-          kode={this.props.kode}
-          noAwal={this.props.noAwal}
-          noAkhir={this.props.noAkhir}
-          in={this.props.in}
-          handler={this.handler}
-          ref={this.child}
-          rendCond={this.props.rendCond}
-        />
+        <p id="logger" className="text-danger">{this.props.err}</p>
+        <br />
+        <label htmlFor="output"><b>Hasil</b></label>
+        <br />
+        <textarea id="out" name="output" rows="10" cols="75" disabled></textarea>
+        <br />
+        <button onClick={this.clr} type="button" name="clear" className="btn btn-danger">Clear</button>
+        <button onClick={this.copyContent} type="button" name="copy" className="btn btn-info">Copy</button>
       </>
     );
   }
@@ -170,6 +238,10 @@ const mapDispatchToProps = (dispatch) => {
     },
     rubahRender : () => {
       const action = { type: "rubahRender" }
+      dispatch(action);
+    },
+    handleError : (value) => {
+      const action = { type: "handleError", value: value }
       dispatch(action);
     }
   }
--- src/components/store/index.js
+++ src/components/store/index.js
@@ -6,6 +6,7 @@ const initialState = {
   noAwal: "",
   noAkhir: "",
   in: "",
+  err: "",
   rendCond: false
 };
 
@@ -15,6 +16,8 @@ const reducer = (state = initialState, action) => {
       return Object.assign({}, state, { [action.store]: action.value });
     case "rubahRender":
       return Object.assign({}, state, { rendCond: !state.rendCond });
+    case "handleError":
+      return Object.assign({}, state, { err: action.value });
     default:
       return state;
   }
